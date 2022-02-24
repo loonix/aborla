4a79c6abc31f325b7d98c6f91b4563fea736e29b
Daniel Carneiro <git-commit-address>
2022-02-24 21:00:36 +0000

    chore: added adapters from models to hive db

--- app/lib/core/storage/hive.dart
+++ app/lib/core/storage/hive.dart
@@ -1,4 +1,7 @@
 
+import 'package:ABorla/core/models/ad_package.dart';
+import 'package:ABorla/core/models/category.dart';
+import 'package:ABorla/core/models/product.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
@@ -19,7 +22,10 @@ Future cleanupHive() async {
 
 void registerHiveAdapters() async {
   try {
-    // Hive.registerAdapter(UserAdapter());
+    Hive.registerAdapter(ProductAdapter());   // 1
+    Hive.registerAdapter(CategoryAdapter());  // 2
+    Hive.registerAdapter(AdPackageAdapter()); // 3
+    
   } catch (e, stackTrace) {
     print(e.toString() + stackTrace.toString());
   }
