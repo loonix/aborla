2857f6492f6af1899744bce859a42628a83f26f8
Daniel Carneiro <git-commit-address>
2022-04-24 18:50:35 +0100

    chore: created form group component

---
+++ web/src/app/shared/form-group/form-group.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'hub-form-group',
+  templateUrl: './form-group.component.html',
+  styleUrls: ['./form-group.component.scss']
+})
+export class FormGroupComponent implements OnInit {
+
+  @Input()
+  control: FormControl = new FormControl();
+  // formControlName of the input.
+  @Input()
+  name: string;
+  @Input()
+  label: string;
+  @Input()
+  validationErrors: { [key: string]: string } = {};
+  @Input()
+  tooltipText?: string;
+  @Input()
+  tooltipPlacement?: string;
+
+  required: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // We run the validator against a form control with an empty value and check if the required attribute exists on the result :)
+    if (this.control.validator && this.control.validator(new FormControl(''))) {
+      const errors = this.control.validator(new FormControl(''));
+      this.required = Object.keys(errors).includes('required');
+    } else {
+      this.required = false;
+    }
+  }
+}
