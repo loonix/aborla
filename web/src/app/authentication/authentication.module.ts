fbe8a3ff0df3ce1bc08e74f9aff1dace4a57aba0
Daniel Carneiro <geral4x@gmail.com>
2022-06-28 23:21:24 +0100

    chore: added login,logout,forgotpassword,emailverification

---
+++ web/src/app/authentication/authentication.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SharedModule } from '@shared';
+import { MaterialModule } from '@app/material.module';
+import { I18nModule } from '@app/i18n';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+// import { AuthRoutingModule } from './auth-routing.module';
+// import { LoginComponent } from './login.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    SharedModule,
+    FlexLayoutModule,
+    MaterialModule,
+    I18nModule,
+    // AuthRoutingModule,
+  ],
+  declarations: [DashboardComponent, ForgotPasswordComponent, SignInComponent,SignUpComponent,VerifyEmailComponent],
+})
+export class AuthenticationModule {}
---
+++ web/src/app/authentication/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@app/@shared/services/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+
+export class DashboardComponent implements OnInit {
+  isActive = true;
+
+  user:any;
+  constructor(public authService: AuthService) {
+    this.user = this.authService.GetUser();
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.authService.isLoggedIn);
+    console.log(this.user);
+  }
+}
