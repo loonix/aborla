0a3e28a5708bf1bbc2719a97ea3f9a734927e685
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2022-07-01 14:42:45 +0100

    fix: authguard

--- web/src/app/@shared/guards/auth.guard.ts
+++ web/src/app/@shared/guards/auth.guard.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-} from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class AuthGuard implements CanActivate {
   constructor(public authService: AuthService, public router: Router) {}
 
