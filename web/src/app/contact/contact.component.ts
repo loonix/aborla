09a61f1d8915ff62d867f35a9126d11523ae0505
Bruno Faria <bruno.faria@netcabo.pt>
2022-07-02 16:06:21 +0100

    chore: html formulario contato - done!

--- web/src/app/contact/contact.component.ts
+++ web/src/app/contact/contact.component.ts
@@ -1,20 +1,39 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list'; 
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+  border: string;
+
+}
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
 export class ContactComponent implements OnInit {
   contactForm: FormGroup;
   disabledSubmitButton: boolean = true;
-
+  tiles: Tile[] = [
+    {text: 'Tile 1', cols: 2, rows: 1 ,border: '3px white'},
+    {text: 'Tile 2', cols: 2, rows: 1 ,border: '3px white'},
+    {text: 'Tile 3', cols: 2, rows: 1 ,border: '3px white'},
+    {text: 'Tile 4', cols: 2, rows: 1 ,border: '3px white'},
+    ];
+  
   @HostListener('input') oninput() {
     if (this.contactForm.valid) {
       this.disabledSubmitButton = false;
     }
+
   }
+  
 
   constructor(
     private fb: FormBuilder,
@@ -40,5 +59,6 @@ export class ContactComponent implements OnInit {
     // });
     console.log(this.contactForm.value);
   }
-
+  
+  
 }
