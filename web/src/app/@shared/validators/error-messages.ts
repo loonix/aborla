eefbcea1ccdfc676848655272a708ade7c0dbdbb
Daniel Carneiro <geral4x@gmail.com>
2022-07-06 00:24:07 +0100

    chore: validation fixes

---
+++ web/src/app/@shared/validators/error-messages.ts
@@ -0,0 +1,13 @@
+
+// Error messages to be displayed in the UI throughout the application.
+export const errorMessages = {
+    required: 'This field is required.',
+    duplicateEmail: 'This email address already exists.',
+    emailAddressMustBeUnique: 'The email address already exists.',
+    emailInvalidShortMessage: 'Invalid email address.',
+    emailInvalid: 'The email address is invalid. Please enter a valid email address.',
+    passwordMinLength: 'The password must be a minimum of 6 characters.',
+    passwordValidation: 'The password needs to be at least 6 characters long and contain lowercase, uppercase and a number',
+    passwordDoNotMatch: "The passwords don't match",
+  };
+  
