7512dba744ed27ad82ec153ee10403003e2cf5de
Bruno Faria <bruno.faria@netcabo.pt>
2022-07-09 00:26:36 +0100

    chore: add coming soon page

--- web/src/app/my-cards/my-cards.component.ts
+++ web/src/app/my-cards/my-cards.component.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from '@angular/fire/analytics';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute } from '@angular/router';
-import { SeoService } from '@app/@shared/seo.service';
-import { AddEditListItemComponent } from '@app/feed/add-edit-list-item/add-edit-list-item.component';
-import { DetailPageComponent } from '@app/feed/detail-page/detail-page.component';
-
 @Component({
   selector: 'app-my-cards',
   templateUrl: './my-cards.component.html',
@@ -14,35 +6,9 @@ import { DetailPageComponent } from '@app/feed/detail-page/detail-page.component
 })
 export class MyCardsComponent implements OnInit {
 
-  itemId: any;
-  item: Item | any;
-
-
-  constructor(
-    private route: ActivatedRoute,
-    private db: AngularFirestore,
-    private seo: SeoService,
-    public dialog: MatDialog
-  ) {}
+  constructor() {}
 
   ngOnInit() {
   }
 
-  
-  onEdit(item: Item): void {
-    const dialogRef = this.dialog.open(AddEditListItemComponent, {
-      data: {
-        isEdit: true,
-        item: item,
-      },
-    });
-  }
-  onView(item: Item): void {
-    const dialogRef = this.dialog.open(DetailPageComponent, {
-      data: {
-        isEdit: true,
-        item: item,
-      },
-    });
-  }
 }
