f96cba0127b597a8d40a1d143fe9b4c04ea917fd
Daniel Carneiro <geral4x@gmail.com>
2022-07-20 01:06:50 +0100

    fix: contact user using user id instead of mock name

--- web/src/app/feed/contact-user/contact-user.component.ts
+++ web/src/app/feed/contact-user/contact-user.component.ts
@@ -1,28 +1,41 @@
 import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Item } from '@app/@shared/models/item.model';
+import { User } from '@app/@shared/models/user.model';
 
 @Component({
   selector: 'app-contact-user',
   templateUrl: './contact-user.component.html',
   styleUrls: ['./contact-user.component.scss'],
 })
-export class ContactUserComponent  implements OnInit {
+export class ContactUserComponent implements OnInit {
   isEdit: boolean;
   item: Item;
   modalTitle: any;
   itemImagesAvaliable: boolean;
   userItems = new FormControl('');
   userItemsList: any = [];
-
+  users: User[];
+  isLoading = true;
   constructor(
     @Optional() public dialogRef: MatDialogRef<ContactUserComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public db: AngularFirestore,
   ) {
     this.setData(data);
   }
-   ngOnInit(): void {
+  ngOnInit(): void {
+    const $obs = this.db
+      .collection('users')
+      .valueChanges({ idField: 'id' });
+
+    $obs.subscribe((users: any) => {
+      this.users = users;
+      this.isLoading = false;
+    });
+
   }
 
   setData(data: any) {
@@ -35,4 +48,9 @@ export class ContactUserComponent  implements OnInit {
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  getUserFromId(id: string) {
+    return this.users.find((user) => user.uid === id);
+  }
+
 }
